066de41e4feb229eee062cb45d8c79e9c0752f8b
zone57 <z83316930@gmail.com>
2026-03-30 16:14:10 +0900

    start

---
+++ android/app/src/main/kotlin/com/example/new22/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/11.dart
@@ -0,0 +1,6 @@
+void main(){
+  String firstname ='liu';
+  String sirname = 'tong';
+  String fullname ='$sirname $firstname';
+  print('Name is $fullname');
+}
